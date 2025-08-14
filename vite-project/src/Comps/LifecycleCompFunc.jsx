cb52dfa5eaad244b456d355fe23514968a17fa28
Aliaksei Leanovich <lleonovalexeyl@gmail.com>
2025-08-14 16:16:36 +0300

    feat: Class and Func Comps were created

---
+++ vite-project/src/Comps/LifecycleCompFunc.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+const LifecycleCompFunc = () => {
+  const [count, setCount] = useState(0);
+  const handleClickIncrement = () => {
+    return setCount((prev) => prev + 1);
+  };
+  useEffect(() => {
+    return async function dataRequest() {
+      const response = await fetch(
+        "https://todo-redev.herokuapp.com/api/users"
+      );
+      const data = await response.json();
+      console.log({ data });
+      console.log("FuncComp-componentDidMount");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("FuncComp-Данные обновились!");
+    console.log(`${count}`);
+  }, [count]);
+  useEffect(() => {
+    return () => console.log("componentWillUnmount");
+  }, []);
+  return (
+    <div>
+      <p>FuncComp</p>
+      <p>Количество: {count}</p>
+      <button onClick={handleClickIncrement}>+++</button>
+    </div>
+  );
+};
+
+export default LifecycleCompFunc;
